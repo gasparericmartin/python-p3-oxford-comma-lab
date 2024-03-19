eb7270df69762cbf9b74d32cc6e7e0fa63ebb178
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-03-18 21:51:39 -0400

    Complete assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,18 @@
 def oxford_comma(items):
-    return None
+    oxford_sentence = ''
+
+
+
+    if len(items) < 2:
+        return oxford_sentence + items[0]
+    elif len(items) < 3:
+        return items[0] + ' and ' + items[1]
+    else:
+        for item in items:
+            if items.index(item) == len(items) - 1:
+                oxford_sentence += f'and {item}'
+            else:
+                oxford_sentence += f'{item}, '
+    
+        return oxford_sentence
+
